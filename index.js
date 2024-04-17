af5b0834661b0a912112d1ebf5e07823c16c11e3
pedester321 <pedro.castro.95@outlook.com>
2024-04-16 22:03:12 -0300

    trabalhando no css

--- index.js
+++ index.js
@@ -13,16 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
     if(productList){   
         database.getProducts().forEach((product , index) => { // Para todo produto e seu indice
             const productElement = document.createElement('div')
-            productElement.classList.add('product-item')
+            productElement.classList.add('product-card')
     
             // Cria uma acora para todos os elementos
             const productLink = document.createElement('a')
             // Aponta o atributo href para pagina produto-detalhe com o seu indice
             productLink.href = `produto-detalhe.html?id=${index}`
             productLink.innerHTML = `
-                <div class="product-name">${product.name}</div>
-                <div class="product-price">${product.price}</div>
-                <div class="product-description">${product.description}</div>
+                <div class="product-image" style="background-image: url('${product.imageUrl}');"></div>
+                <div class="product-info">
+                    <h3>${product.name}</h3>
+                    <p>${product.description}</p>
+                    <p>${product.price}</p>
+                </div>
             `
     
             // Adiciona a ancora ao elemento html na tela
